9722efd50e45ae2f0853c98a3cc79280d5b40713
luisusr <ziul09@gmail.com>
2017-06-26 12:01:04 +0200

    Add files via upload

---
+++ msdemo/src/main/java/com/example/ms/MsdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdemoApplication.class, args);
+	}
+}
